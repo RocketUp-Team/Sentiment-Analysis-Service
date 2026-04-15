6bfe4b6d7fe7db608fb725ebf97c4b8e6bc03f14
TrungDQ <trungdq.ts@gmail.com>
2026-04-15 15:44:08 +0700

    fix: align data utility test dependencies

--- tests/data/test_utils.py
+++ tests/data/test_utils.py
@@ -1,4 +1,3 @@
-import os
 import yaml
 from src.data.utils import load_params
 
@@ -6,7 +5,7 @@ from src.data.utils import load_params
 def test_load_params(tmp_path):
     config_file = tmp_path / "test_params.yaml"
     config_data = {"data": {"split_seed": 42}}
-    with open(config_file, "w") as f:
+    with open(config_file, "w", encoding="utf-8") as f:
         yaml.dump(config_data, f)
 
     params = load_params(str(config_file))
